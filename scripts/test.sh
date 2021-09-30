50f2b617f6386ac010f8dc58dc5768dbae730de1
kaali <kaliraja1992@gmail.com>
2021-09-30 16:29:53 +0530

    updated the action WF

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,9 @@ echo "Hi Kali! How are you doing?"
 
 mkdir -p ~/.kube
 echo $kube_config | base64 -d > ~/.kube/config
-apt update
-apt install curl
+apk --update add curl gettext
 export KUBECONFIG=~/.kube/config
-curl -L -o /usr/local/bin/kubectl https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl
-#curl -L -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+#curl -L -o /usr/local/bin/kubectl https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl
+curl -L -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod a+x /usr/local/bin/kubectl
 kubectl get ns
