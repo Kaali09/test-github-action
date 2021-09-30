7e0d4130d39523ebeb97cb0d1cdd21cfc92c1924
kaali <kaliraja1992@gmail.com>
2021-09-30 16:12:34 +0530

    updated the action WF

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "Hi Kali! How are you doing?"
+
+mkdir -p ~/.kube
+echo $kube_config | base64 -d > ~/.kube/config
+
+export KUBECONFIG=~/.kube/config
+curl -L -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod a+x /usr/local/bin/kubectl
+kubectl get ns
